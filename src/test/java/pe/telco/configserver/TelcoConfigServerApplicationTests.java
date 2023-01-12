5710fb1d030f1b67cf2df22279b46954aa11a4c9
rodcar <ivanrod12@hotmail.com>
2023-01-11 21:09:59 -0500

    commit inicial

---
+++ src/test/java/pe/telco/configserver/TelcoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.telco.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
